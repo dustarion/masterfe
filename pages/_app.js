4e68c2f5357cd89943fa31687793077a6d636d67
Qin Guan <qinguan20040914@gmail.com>
2019-03-04 23:35:25 +0800

    start

---
+++ pages/_app.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App, { Container } from "next/app";
+import withRedux from "next-redux-wrapper";
+
+const reducer = (state = { foo: "" }, action) => {
+  switch (action.type) {
+    case "FOO":
+      return { ...state, foo: action.payload };
+    default:
+      return state;
+  }
+};
+
+const makeStore = (initialState, options) => {
+  return createStore(reducer, initialState);
+};
+
+class MasterFE extends App {
+  static async getInitialProps({ Component, ctx }) {
+    // we can dispatch from here too
+    ctx.store.dispatch({ type: "FOO", payload: "foo" });
+
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
+}
+
+export default withRedux(makeStore)(MasterFE);
