f2ceb44425dae853f1eea69b1e93a1c27e3bf7ae
Qin Guan <qinguan20040914@gmail.com>
2019-03-09 22:32:22 +0800

    fuck

--- components/CockpitTestInput/index.js
+++ components/CockpitTestInput/index.js
@@ -2,35 +2,30 @@ import React, { Component } from "react";
 import style from "./index.css";
 
 class CockpitTestInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-      }
+  constructor(props) {
+    super(props);
+    this.state = { answer: "" };
+  }
 
-    render() {
-        return (
-                <div className={style.container}>
-                {/* <form onSubmit={this.handleSubmit}> */}
-                {/* <label> */}
-                <input className={style.textInput} type="text" value={this.state.value} onChange={this.handleChange} />
-                {/* </label> */}
-                {/* <input type="submit" value="Submit" /> */}
-                {/* </form> */}
-            </div>
-        );
-    }
+  componentDidMount() {
+    document.getElementById("inputanswer").focus();
+  }
+
+  render() {
+    return (
+      <div className={style.container}>
+        <input
+          id="inputanswer"
+          className={style.textInput}
+          type="text"
+          onChange={e => {
+            this.setState({ answer: e.target.value });
+          }}
+        />
+        <span className={style.subText}>type your answer above</span>
+      </div>
+    );
+  }
 }
 
 export default CockpitTestInput;
--- components/TestCard/index.js
+++ components/TestCard/index.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import style from "./index.css";
 import CockpitTestInput from "../CockpitTestInput";
+import validator from "validator";
 
 class TestCard extends Component {
   render() {
     return (
-        <div className={style.container}>
-            <div className={style.contentWrapper}>
-                <h1>What is the main state in which an element’s atoms are in superposition?</h1>
-            </div>
-            <div>
-                <CockpitTestInput />
-            </div>
+      <div className={style.container}>
+        <div className={style.contentWrapper}>
+          <h1 className={style.titleh1}>
+            {this.props.q == undefined || null
+              ? "An error has occurred"
+              : this.props.q}
+          </h1>
         </div>
+        <div>
+          <CockpitTestInput />
+        </div>
+      </div>
     );
   }
 }
