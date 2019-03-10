3d2a52ee7216c79e7cc5649a2cabc4f312441fe5
Dalton Ng <dustarion@gmail.com>
2019-03-10 16:34:49 +0800

    added gradients

---
+++ components/SetsButton/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import style from "./index.css";
+
+class FolderButton extends Component {
+  render() {
+    return (
+      <div className={style.container}>
+        {/* <img src="/static/FolderIcon.png" className={style.folderIcon} /> */}
+        <span className={style.gradientLeftBar}></span>
+        <span className={style.name}>
+          {this.props.title == undefined || null ? "Error" : this.props.title}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default FolderButton;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import style from "./dashboard.css";
 import Header from "../components/Header";
 import FolderButton from "../components/FolderButton";
+import SetsButton from "../components/SetsButton";
 
 class DashboardPage extends Component {
   folders() {
@@ -21,9 +22,13 @@ class DashboardPage extends Component {
             <div className={style.folderCard}>
               <span className={style.headerText}>Folders</span>
               {this.folders()}
+              
+              <SetsButton/>
+              <SetsButton/>
             </div>
+            
           </div>
-          <div style={{ flex: 1, background: "white" }}>
+          <div style={{ flex: 1, background: "grey" }}>
             everything else here
           </div>
         </div>
