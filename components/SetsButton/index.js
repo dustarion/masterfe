01d2e6bddce7c387f61d4ccd772d19fcf16864d8
Dalton Ng <dustarion@gmail.com>
2019-03-10 17:07:34 +0800

    fixed some shit

--- components/SetsButton/index.js
+++ components/SetsButton/index.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import style from "./index.css";
 
-class FolderButton extends Component {
+
+class SetsButton extends Component {
   render() {
     return (
       <div className={style.container}>
         {/* <img src="/static/FolderIcon.png" className={style.folderIcon} /> */}
-        <span className={style.gradientLeftBar}></span>
+        <div className={style.gradientLeftBar} />
         <span className={style.name}>
           {this.props.title == undefined || null ? "Error" : this.props.title}
         </span>
+
       </div>
     );
   }
 }
 
-export default FolderButton;
+export default SetsButton;
