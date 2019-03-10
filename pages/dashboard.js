bd02f14a031c6e8a36cf48698599cda32415839b
Qin Guan <qinguan20040914@gmail.com>
2019-03-10 13:32:03 +0800

    woah

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,13 +4,28 @@ import Header from "../components/Header";
 import FolderButton from "../components/FolderButton";
 
 class DashboardPage extends Component {
+  folders() {
+    return <FolderButton title="Rocket shit" />;
+  }
   render() {
     return (
       <div className={style.container}>
         <Header />
-        <div className={style.folderCard}>
-            <span className={style.headerText}>Folders</span>
-            <FolderButton />
+        <div
+          style={{
+            flex: 1,
+            display: "flex"
+          }}
+        >
+          <div>
+            <div className={style.folderCard}>
+              <span className={style.headerText}>Folders</span>
+              {this.folders()}
+            </div>
+          </div>
+          <div style={{ flex: 1, background: "white" }}>
+            everything else here
+          </div>
         </div>
       </div>
     );
