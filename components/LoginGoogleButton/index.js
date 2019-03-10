9abe83ae9a914e603c4e9e59235be13ef065f17b
Qin Guan <qinguan20040914@gmail.com>
2019-03-10 21:49:49 +0800

    fuck

---
+++ components/LoginGoogleButton/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Link from "next/link";
+
+class LoginGoogleButton extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default LoginGoogleButton;
