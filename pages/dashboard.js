70a8d3a0eee4baba7c6e5e5e2e32eed6045bda6d
Qin Guan <qinguan20040914@gmail.com>
2019-03-10 12:03:27 +0800

    gay

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import style from "./dashboard.css";
 import Header from "../components/Header";
-import DashboardNavCard from "../components/DashboardNavCard";
 
 class DashboardPage extends Component {
   render() {
     return (
       <div className={style.container}>
         <Header />
-        <div className={style.menucard}>
-            <DashboardNavCard/>
-        </div>
       </div>
     );
   }
