55fb09c05d6a1fd42574380acc21d4009792ea86
Dalton Ng <dustarion@gmail.com>
2019-03-10 12:04:31 +0800

    banana

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,7 +9,7 @@ class DashboardPage extends Component {
       <div className={style.container}>
         <Header />
         <div className={style.menucard}>
-            <DashboardNavCard/>
+            hello
         </div>
       </div>
     );
