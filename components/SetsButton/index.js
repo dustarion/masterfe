98dd3b3211395730190fe26be2804fbfde64523a
Dalton Ng <dustarion@gmail.com>
2019-03-11 00:02:09 +0800

    am dead tired i suck at node

--- components/SetsButton/index.js
+++ components/SetsButton/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import style from "./index.css";
 
-
 class SetsButton extends Component {
   render() {
     return (
       <div className={style.container}>
-        {/* <img src="/static/FolderIcon.png" className={style.folderIcon} /> */}
-        <div className={style.gradientLeftBar} />
+        <span className={style.gradientLeftBar}>
+        </span>
+        
         <span className={style.name}>
           {this.props.title == undefined || null ? "Error" : this.props.title}
+        </span>
+
+        <span className={style.progressText}>
+          {this.props.progress == undefined || null ? "0%" : this.props.progress}
         </span>
 
       </div>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -6,8 +6,25 @@ import SetsButton from "../components/SetsButton";
 
 class DashboardPage extends Component {
   folders() {
-    return <FolderButton title="Rocket shit" />;
+    return (
+      <div>
+        <FolderButton title="Rocket Science" />
+        <FolderButton title="Cognitive Psychology" />
+        <FolderButton title="Quantum Physics" />
+      </div>
+    );
   }
+
+  sets() {
+    return (
+      <div>
+        <SetsButton title="Introduction to Organic Chemistry" progress="66%"/>
+        <SetsButton title="Molecules" progress="66%"/>
+        <SetsButton title="Flux Capacitor and Temporal Displacement" progress="66%"/>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={style.container}>
@@ -22,14 +39,16 @@ class DashboardPage extends Component {
             <div className={style.folderCard}>
               <span className={style.headerText}>Folders</span>
               {this.folders()}
-              
-              <SetsButton/>
-              <SetsButton/>
             </div>
             
           </div>
-          <div style={{ flex: 1, background: "grey" }}>
-            everything else here
+          <div className={style.contentWrapper}>
+            <div className={style.recentCard}>
+              <span className={style.headerText}>In Progress</span>
+              {this.sets()}
+              <span className={style.headerText}>Last Week</span>
+              {this.sets()}
+            </div>
           </div>
         </div>
       </div>
