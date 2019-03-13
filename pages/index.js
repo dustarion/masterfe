efe5d1ba464d4145dd33390503100ab037a4eb77
Qin Guan <qinguan20040914@gmail.com>
2019-03-13 08:45:55 +0800

    fuck

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import style from "./index.css";
+import SolidButton from "../components/SolidButton";
 
 class IndexPage extends Component {
   render() {
     return (
       <div className={style.container}>
         <Header />
+        <section className={style.section} style={{ justifyContent: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column"
+            }}
+          >
+            <h1 className={style.h2}>
+              Use Master.
+              <br />
+              Study Faster.
+            </h1>
+            <span className={style.subtitle}>
+              Get good grades without breaking yourself
+            </span>
+            <div style={{ width: 250, marginTop: 20 }}>
+              <SolidButton
+                color="black"
+                text="get started for free"
+                fontColor="white"
+                href="/signup"
+                fontSize={14}
+                size="small"
+              />
+            </div>
+          </div>
+          <div style={{ flex: 1 }} />
+        </section>
       </div>
     );
   }
